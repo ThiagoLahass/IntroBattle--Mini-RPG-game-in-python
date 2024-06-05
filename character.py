7faab9c0d66a0c01d2cfc9e3b0736b3b7777fe68
Thiago Lahass <lahassthiagofelippe@gmail.com>
2024-06-05 01:31:01 -0300

    :memo:Creating docstrings for character class functions

--- character.py
+++ character.py
@@ -1,7 +1,32 @@
 import pygame
 
 class Character:
+    """
+    A class representing a character in the game.
+
+    Attributes:
+        name (str): The name of the character.
+        hp (int): The current health points of the character.
+        max_hp (int): The maximum health points of the character.
+        attack (int): The attack power of the character.
+        defense (int): The defense power of the character.
+        speed (int): The speed of the character.
+        image (pygame.Surface): The image representing the character.
+        is_defending (bool): Whether the character is defending in the current turn.
+    """
+
     def __init__(self, name, hp, attack, defense, speed, image_path):
+        """
+        Initializes a Character object.
+
+        Args:
+            name (str): The name of the character.
+            hp (int): The health points of the character.
+            attack (int): The attack power of the character.
+            defense (int): The defense power of the character.
+            speed (int): The speed of the character.
+            image_path (str): The file path of the character's image.
+        """
         self.name = name
         self.hp = hp
         self.max_hp = hp
@@ -13,14 +38,23 @@ class Character:
         self.is_defending = False
 
     def take_damage(self, damage):
-        '''
-        
-        '''
+        """
+        Reduces the character's health points by the specified damage amount.
+
+        Args:
+            damage (float): The amount of damage to be taken.
+        """
         self.hp -= damage
         if self.hp < 0:
             self.hp = 0
 
     def attack_target(self, target):
+        """
+        Attacks the target character, causing damage based on the attacker's attack and the target's defense.
+
+        Args:
+            target (Character): The target character to be attacked.
+        """
         damage = self.attack * (50 / (50 + target.defense))
         if target.is_defending:
             damage /= 2
